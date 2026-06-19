999f982819b514c1c86a6410bf898d60ceeccda5
tahoangphuc <tahoangphuc111@gmail.com>
2026-06-19 11:23:24 +0700

    hotfix: mathjax v4.1.2

--- resources/mathjax_config.js
+++ resources/mathjax_config.js
@@ -10,12 +10,25 @@ window.MathJax = {
         enableSpeech: false,
         enableBraille: false
     },
+    chtml: {
+        fontURL: '/static/mathjax/mathjax-newcm/woff2',
+        dynamicPrefix: '/static/mathjax/mathjax-newcm/dynamic'
+    },
     loader: {
         paths: {
-            'mathjax-newcm': '/static/mathjax/mathjax-newcm/chtml/dynamic'
-        }
+            'mathjax-newcm': '/static/mathjax/mathjax-newcm'
+        },
+        pathFilters: [
+            function (data) {
+                if (data.name.indexOf('[mathjax-newcm]/chtml/dynamic/') === 0) {
+                    data.name = data.name.replace('[mathjax-newcm]/chtml/dynamic/', '[mathjax-newcm]/dynamic/');
+                }
+                return true;
+            }
+        ]
     },
     output: {
         font: 'mathjax-newcm'
     }
 };
+
