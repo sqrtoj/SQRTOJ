ff694cef9ce3eac7a7f8d4924d4605eb08bdcc69
tahoangphuc <tahoangphuc111@gmail.com>
2026-06-19 10:39:37 +0700

    feat(math): upgrade MathJax to v4.1.2

--- resources/mathjax_config.js
+++ resources/mathjax_config.js
@@ -6,6 +6,16 @@ window.MathJax = {
         ]
     },
     options: {
-        enableMenu: false
+        enableMenu: false,
+        enableSpeech: false,
+        enableBraille: false
+    },
+    loader: {
+        paths: {
+            'mathjax-newcm': '/static/mathjax/mathjax-newcm/chtml/dynamic'
+        }
+    },
+    output: {
+        font: 'mathjax-newcm'
     }
 };
