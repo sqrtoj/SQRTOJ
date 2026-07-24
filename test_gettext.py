e4ad16b130140ff4c0547daf28703c79128ceea4
tahoangphuc111 <tahoangphuc111@gmail.com>
2026-07-24 19:19:06 +0700

    fix lint

--- test_gettext.py
+++ test_gettext.py
@@ -1,7 +1,12 @@
-import os, django
+import datetime
+import os
+
+import django
+from django.utils.translation import gettext as _
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sqrtojsite.settings')
 django.setup()
-from django.utils.translation import gettext as _
-import datetime
+
 td = datetime.timedelta(days=5, hours=21, minutes=47, seconds=46)
 print(_('Contest ends in %(countdown)s.') % {'countdown': td})
